d94b4df8314af13aff1bb81bf5937bca4b88dcf6
Sofiane Soussi <sofiane@myrspoven.com>
2021-05-16 00:42:41 +0200

    fixed config file

--- src/experimentation/tune_config_parsing.py
+++ src/experimentation/tune_config_parsing.py
@@ -16,8 +16,11 @@ def tune_parse_config_dict(config_raw: Dict[str, Any]) -> Dict[str, Any]:
     parsed_config = dict()
     parsed_config["network"] = tune.choice(config_raw["network"])
     parsed_config["model_params"] = tune.choice([json.dumps(p) for p in config_raw["model_params"]])
-    parsed_config["lr_extraction"] = tune.loguniform(*config_raw["lr_extraction"])
-    parsed_config["lr_tuning"] = tune.loguniform(*config_raw["lr_tuning"])
+    for lr in ["lr_extraction", "lr_tuning"]:
+        if len(config_raw[lr]) == 1:
+            parsed_config[lr] = config_raw[lr]
+        else:
+            parsed_config[lr] = tune.loguniform(*config_raw[lr])
     parsed_config["loss"] = config_raw["loss"]
     parsed_config["batch_size"] = tune.choice(config_raw["batch_size"])
     parsed_config["val_fraction"] = config_raw["val_fraction"]
