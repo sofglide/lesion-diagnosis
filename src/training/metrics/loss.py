6dda0f3257e034264ea91b717039fd8c9be851e7
Sofiane Soussi <sofiane@myrspoven.com>
2021-05-16 22:32:03 +0200

    fix bug in get_criterion

--- src/training/metrics/loss.py
+++ src/training/metrics/loss.py
@@ -16,14 +16,15 @@ def get_criterion(loss_params: Dict[str, Any]) -> nn.Module:
     :param loss_params:
     :return:
     """
-    loss_function = loss_params.pop("function", "cross_entropy")
-    if "weight" in loss_params:
-        loss_params["weight"] = torch.FloatTensor(loss_params["weight"])
+    loss_params_local = loss_params.copy()
+    loss_function = loss_params_local.pop("function", "cross_entropy")
+    if "weight" in loss_params_local:
+        loss_params_local["weight"] = torch.FloatTensor(loss_params_local["weight"])
 
     if loss_function == "cross_entropy":
-        criterion: nn.Module = nn.CrossEntropyLoss(**loss_params)
+        criterion: nn.Module = nn.CrossEntropyLoss(**loss_params_local)
     elif loss_function == "focal_loss":
-        criterion = FocalLoss(**loss_params)
+        criterion = FocalLoss(**loss_params_local)
     else:
         raise NotImplementedError(f"unknown loss function {loss_function}")
 
