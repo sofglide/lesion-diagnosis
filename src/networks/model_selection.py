66b2a8016dcece3ab59eb543a8b5a447aa9d07de
Sofiane Soussi <sofiane@myrspoven.com>
2021-05-16 02:14:09 +0200

    fixed model selection for hybrid

--- src/networks/model_selection.py
+++ src/networks/model_selection.py
@@ -7,6 +7,7 @@ from torch.backends import cudnn
 
 from networks.base_net import BaseNet
 from networks.densenet import Densenet
+from networks.hybrid import Hybrid
 from networks.resnet import Resnet
 from networks.simple_cnn import SimpleCNN
 from utils.computing_device import get_device
@@ -31,8 +32,8 @@ def get_model(network: str, num_classes: int, model_params: Optional[Dict[str, A
         net = Resnet(num_classes)
     elif network == "Densenet":
         net = Densenet(num_classes)
-    # elif network == "Hybrid":
-    #     net = Hybrid(num_classes, params=model_params)
+    elif network == "Hybrid":
+        net = Hybrid(num_classes, params=model_params)
     else:
         raise ValueError(f"unknown network {network}")
     net = net.to(device)
